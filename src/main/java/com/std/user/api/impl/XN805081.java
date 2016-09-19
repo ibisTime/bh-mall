5ecb99abbbec8e57b751a25518bb36ef065f1f5d
pyez1158 <pyez1158@163.com>
2016-09-19 14:01:04 +0800

    是否为空校验

--- src/main/java/com/std/user/api/impl/XN805081.java
+++ src/main/java/com/std/user/api/impl/XN805081.java
@@ -3,6 +3,7 @@ package com.std.user.api.impl;
 import com.std.user.ao.IUserRelationAO;
 import com.std.user.api.AProcessor;
 import com.std.user.common.JsonUtil;
+import com.std.user.core.StringValidater;
 import com.std.user.dto.req.XN805081Req;
 import com.std.user.dto.res.BooleanRes;
 import com.std.user.exception.BizException;
@@ -30,5 +31,6 @@ public class XN805081 extends AProcessor {
     @Override
     public void doCheck(String inputparams) throws ParaException {
         req = JsonUtil.json2Bean(inputparams, XN805081Req.class);
+        StringValidater.validateBlank(req.getUserId(), req.getToUser());
     }
 }
