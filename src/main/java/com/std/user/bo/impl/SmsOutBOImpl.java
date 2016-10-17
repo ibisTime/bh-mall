adbad790b88b1141858a7b5de30f42be737985e6
pyez1158 <pyez1158@163.com>
2016-10-17 19:19:21 +0800

    发送短信bug修复

--- src/main/java/com/std/user/bo/impl/SmsOutBOImpl.java
+++ src/main/java/com/std/user/bo/impl/SmsOutBOImpl.java
@@ -4,17 +4,12 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.std.user.bo.ISmsOutBO;
-import com.std.user.dto.req.CD799001Req;
-import com.std.user.dto.req.CD799002Req;
+import com.std.user.common.PropertiesUtil;
 import com.std.user.dto.req.XN799001Req;
-import com.std.user.dto.req.XN799002Req;
 import com.std.user.dto.req.XN799003Req;
+import com.std.user.dto.req.XN799007Req;
 import com.std.user.dto.res.BooleanRes;
 import com.std.user.dto.res.PKCodeRes;
-import com.std.user.dto.res.XN799001Res;
-import com.std.user.dto.res.XN799002Res;
-import com.std.user.dto.res.XN799003Res;
-import com.std.user.exception.BizException;
 import com.std.user.http.BizConnecter;
 import com.std.user.http.JsonUtils;
 
@@ -23,84 +18,42 @@ public class SmsOutBOImpl implements ISmsOutBO {
     static Logger logger = Logger.getLogger(SmsOutBOImpl.class);
 
     @Override
-    public void sendSmsOut(String mobile, String content, String bizType) {
+    public void sendCaptcha(String mobile, String bizType) {
         try {
-            XN799001Req req = new XN799001Req();
+            XN799003Req req = new XN799003Req();
+            req.setChannel(PropertiesUtil.Config.SMS_CHANNEL + "-K");
             req.setMobile(mobile);
-            req.setContent(content);
             req.setBizType(bizType);
-            BizConnecter.getBizData("799001", JsonUtils.object2Json(req),
-                XN799001Res.class);
+            BizConnecter.getBizData("799003", JsonUtils.object2Json(req),
+                PKCodeRes.class);
         } catch (Exception e) {
             logger.error("调用短信发送服务异常");
         }
-
     }
 
     @Override
     public void checkCaptcha(String mobile, String captcha, String bizType) {
-        XN799002Req req = new XN799002Req();
+        XN799007Req req = new XN799007Req();
+        String[] channels = PropertiesUtil.Config.SMS_CHANNEL.split("-");
+        req.setCompanyCode(channels[0]);
         req.setMobile(mobile);
         req.setCaptcha(captcha);
         req.setBizType(bizType);
-        BizConnecter.getBizData("799002", JsonUtils.object2Json(req),
-            XN799002Res.class);
+        BizConnecter.getBizData("799007", JsonUtils.object2Json(req),
+            BooleanRes.class);
     }
 
     @Override
-    public void sendCaptcha(String mobile, String bizType) {
+    public void sendSmsOut(String mobile, String content, String bizType) {
         try {
-            XN799003Req req = new XN799003Req();
-            req.setMobile(mobile);
-            req.setBizType(bizType);
-            BizConnecter.getBizData("799003", JsonUtils.object2Json(req),
-                XN799003Res.class);
-        } catch (Exception e) {
-            logger.error("调用短信验证服务异常");
-            throw new BizException("xn799003", "短信发送异常，请稍后再试");
-        }
-    }
-
-    @Override
-    public String sendSmsOut(String mobile, String content) {
-        String code = null;
-        try {
-            CD799001Req req = new CD799001Req();
-            req.setChannel("GJ1002-CSMD-K");
+            XN799001Req req = new XN799001Req();
+            req.setChannel(PropertiesUtil.Config.SMS_CHANNEL + "-M");
             req.setMobile(mobile);
             req.setContent(content);
-            PKCodeRes res = BizConnecter.getBizData("799001",
-                JsonUtils.object2Json(req), PKCodeRes.class);
-            code = res.getCode();
+            BizConnecter.getBizData("799001", JsonUtils.object2Json(req),
+                PKCodeRes.class);
         } catch (Exception e) {
             logger.error("调用短信发送服务异常");
         }
-        return code;
-    }
-
-    @Override
-    public boolean checkCaptcha(String code, String captcha) {
-        CD799002Req req = new CD799002Req();
-        req.setCode(code);
-        req.setCaptcha(captcha);
-        BooleanRes res = BizConnecter.getBizData("799002",
-            JsonUtils.object2Json(req), BooleanRes.class);
-        return res.getIsSuccess();
-    }
-
-    @Override
-    public String sendCaptcha(String mobile) {
-        String code = null;
-        try {
-            CD799001Req req = new CD799001Req();
-            req.setChannel("GJ1002-CSMD-K");
-            req.setMobile(mobile);
-            PKCodeRes res = BizConnecter.getBizData("799001",
-                JsonUtils.object2Json(req), PKCodeRes.class);
-            code = res.getCode();
-        } catch (Exception e) {
-            logger.error("调用短信发送服务异常");
-        }
-        return code;
     }
 }
