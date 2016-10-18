b648566e18d62aa1a10eb9a4ae8be98dbacf045c
pyez1158 <pyez1158@163.com>
2016-10-18 13:35:18 +0800

    admin管理员重置密码

---
+++ src/main/java/com/std/user/api/impl/XN805078.java
@@ -0,0 +1,33 @@
+package com.std.user.api.impl;
+
+import com.std.user.ao.IUserAO;
+import com.std.user.api.AProcessor;
+import com.std.user.common.JsonUtil;
+import com.std.user.core.StringValidater;
+import com.std.user.dto.req.XN805078Req;
+import com.std.user.exception.BizException;
+import com.std.user.exception.ParaException;
+import com.std.user.spring.SpringContextHolder;
+
+/**
+ * admin管理员重置密码
+ * @author: xieyj 
+ * @since: 2016年10月18日 下午1:34:21 
+ * @history:
+ */
+public class XN805078 extends AProcessor {
+    private IUserAO userAO = SpringContextHolder.getBean(IUserAO.class);
+
+    private XN805078Req req = null;
+
+    @Override
+    public Object doBusiness() throws BizException {
+        return null;
+    }
+
+    @Override
+    public void doCheck(String inputparams) throws ParaException {
+        req = JsonUtil.json2Bean(inputparams, XN805078Req.class);
+        StringValidater.validateBlank(req.getUserId(), req.getAdminPwd());
+    }
+}
