a219e680acd668fbc6a816b23daf8463ab99490e
pyez1158 <pyez1158@163.com>
2016-11-26 16:15:38 +0800

    bug 修复

--- src/main/java/com/std/user/ao/impl/B2cSmsAOImpl.java
+++ src/main/java/com/std/user/ao/impl/B2cSmsAOImpl.java
@@ -8,12 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.std.user.ao.IB2cSmsAO;
 import com.std.user.bo.IB2cSmsBO;
+import com.std.user.bo.ICompanyBO;
 import com.std.user.bo.ISmsOutBO;
 import com.std.user.bo.IUReadBO;
 import com.std.user.bo.IUserBO;
 import com.std.user.bo.base.Paginable;
 import com.std.user.common.PropertiesUtil;
 import com.std.user.domain.B2cSms;
+import com.std.user.domain.Company;
 import com.std.user.domain.User;
 import com.std.user.enums.EB2cSmsType;
 import com.std.user.enums.EBoolean;
@@ -35,6 +37,9 @@ public class B2cSmsAOImpl implements IB2cSmsAO {
 
     @Autowired
     private ISmsOutBO smsOutBO;
+
+    @Autowired
+    private ICompanyBO companyBO;
 
     @Override
     public String addB2cSms(B2cSms data) {
@@ -88,8 +93,10 @@ public class B2cSmsAOImpl implements IB2cSmsAO {
                 PropertiesUtil.Config.MASTER_SECRET, data.getTitle());
         } else if (EB2cSmsType.SMS.getCode().equals(data.getType())) {
             for (User user : userList) {
-                smsOutBO.sendSmsOut(user.getMobile(), data.getTitle(),
-                    "805123", data.getCompanyCode());
+                Company company = companyBO.getCompany(user.getCompanyCode());
+                smsOutBO.sendSmsOut(user.getMobile(), data.getTitle() + "("
+                        + company.getName() + ")", "805123",
+                    data.getCompanyCode());
                 // 统计商户发了多少短信
                 uReadBO.saveURead(code, user.getUserId());
             }
