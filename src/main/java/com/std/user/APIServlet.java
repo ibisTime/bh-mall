23989bbe88ba092bb86c5e82b25ca27c4dba0902
pyez1158 <pyez1158@163.com>
2016-12-17 19:46:11 +0800

    脚本程序整理

--- src/main/java/com/std/user/APIServlet.java
+++ src/main/java/com/std/user/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:1.0.0 \n");
-        writer.append("Description:1st_b \n");
+        writer.append("Description:user_5th \n");
         writer.flush();
     }
 
