e8e2896edbfe45419fbf194ae75e939561639cdd
shan <shan@DESKTOP-CBF9EC5>
2016-12-19 12:22:23 +0800

    邀请码接口写好

--- src/main/java/com/std/user/core/EGeneratePrefix.java
+++ src/main/java/com/std/user/core/EGeneratePrefix.java
@@ -12,7 +12,7 @@ public enum EGeneratePrefix {
     COM("GS", "公司"), UZ("UZ", "用户组"), DH("DH", "导航"), SC("SC", "公司素材"), YX(
             "YX", "合作意向"), PW("PW", "密码记录"), XX("XX", "消息"), ZZ("ZZ", "资质"), GZ(
             "GZ", "公司资质"), CM("CM", "公司菜单"), CB("CB", "公司BANNER"), AD("AD",
-            "用户地址"), KQ("KQ", "卡券");
+            "用户地址"), KQ("KQ", "卡券"), YQ("YQ", "邀请码");
 
     public static Map<String, EGeneratePrefix> getOrderTypeMap() {
         Map<String, EGeneratePrefix> map = new HashMap<String, EGeneratePrefix>();
