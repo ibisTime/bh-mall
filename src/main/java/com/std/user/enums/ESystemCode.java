85db4de864b2f3459078526c4bed3b7bbe3dd75e
asus <asus@DESKTOP-N61H2QE>
2017-06-17 15:42:09 +0800

    公共服务平台不需要账户

--- src/main/java/com/std/user/enums/ESystemCode.java
+++ src/main/java/com/std/user/enums/ESystemCode.java
@@ -20,7 +20,8 @@ public enum ESystemCode {
     ZHPAY("CD-CZH000001", "正汇钱包"), CSH("CD-CCSH00000X", "橙商户"), CAIGO(
             "CD-CCG000007", "菜狗商城"), CSW("CD-CCSW000008", "城市网"), DZT(
             "CD-CDZT000009", "定制通"), YAOCHENG("CD-CYC000009", "姚橙"), PIPE(
-            "CD-CGD000006", "全能水电工助手"), JKYG("CD-JKEG000011", "健康E购");
+            "CD-CGD000006", "全能水电工助手"), JKYG("CD-JKEG000011", "健康E购"), SERVICE(
+            "CD-CJJ000010", "公共服务平台");
 
     public static Map<String, ESystemCode> getCurrencyMap() {
         Map<String, ESystemCode> map = new HashMap<String, ESystemCode>();
