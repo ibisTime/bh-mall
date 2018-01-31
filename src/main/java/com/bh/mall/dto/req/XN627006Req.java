4dfd0da5f8b93f8927695b0a6fb55efb19959353
chenshan <chenshan@localhost>
2018-01-31 17:29:57 +0800

    脚本更新

--- src/main/java/com/bh/mall/dto/req/XN627006Req.java
+++ src/main/java/com/bh/mall/dto/req/XN627006Req.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.bh.mall.dto.req;
 
 /**
@@ -32,37 +31,3 @@ public class XN627006Req {
     }
 
 }
-=======
-package com.bh.mall.dto.req;
-
-/**
- * 列表查询代理
- * @author nyc
- *
- */
-public class XN627006Req {
-
-    // 等级名称 （选填）
-    private String level;
-
-    // 等级名称 （选填）
-    private String name;
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-}
->>>>>>> refs/remotes/origin/master
