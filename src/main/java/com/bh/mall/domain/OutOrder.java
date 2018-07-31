0c71d6af342005fc52c77342e4801f526a4c9569
nyc <yachao.ning@hichengdai.com>
2018-07-31 11:49:23 +0800

    修改

---
+++ src/main/java/com/bh/mall/domain/OutOrder.java
@@ -0,0 +1,658 @@
+package com.bh.mall.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import com.bh.mall.dao.base.ABaseDO;
+
+/***
+ * 出货订单
+ * @author: nyc 
+ * @since: 2018年7月31日 上午10:29:37 
+ * @history:
+ */
+
+public class OutOrder extends ABaseDO {
+
+    private static final long serialVersionUID = 1L;
+
+    // 编号
+    private String code;
+
+    // 是否云仓发货
+    private String isCompanySend;
+
+    // 分类
+    private String kind;
+
+    // 产品编号
+    private String productCode;
+
+    // 产品名称
+    private String productName;
+
+    // 规格编号
+    private String specsCode;
+
+    // 规格名称
+    private String specsName;
+
+    // 关联箱码
+    private String proCode;
+
+    // 图片
+    private String pic;
+
+    // 数量
+    private Integer quantity;
+
+    // 单价
+    private Long price;
+
+    // 向谁提货
+    private String toUser;
+
+    // 单价
+    private Long amount;
+
+    // 运费
+    private Long yunfei;
+
+    // 状态
+    private String status;
+
+    // 支付渠道
+    private String payType;
+
+    // 支付组号
+    private String payGroup;
+
+    // 支付金额
+    private Long payAmount;
+
+    // 支付时间
+    private Date payDatetime;
+
+    // 渠道编号
+    private String payCode;
+
+    // 下单人
+    private String applyUser;
+
+    // 下单备注
+    private String applyNote;
+
+    // 下单时间
+    private Date applyDatetime;
+
+    // 收件人姓名
+    private String signer;
+
+    // 收件人电话
+    private String mobile;
+
+    // 省
+    private String province;
+
+    // 市
+    private String city;
+
+    // 区
+    private String area;
+
+    // 收货地址
+    private String address;
+
+    // 发货人
+    private String deliver;
+
+    // 发货时间
+    private Date deliveDatetime;
+
+    // 物流单号
+    private String logisticsCode;
+
+    // 物流公司
+    private String logisticsCompany;
+
+    // 更新人
+    private String updater;
+
+    // 更新时间
+    private Date updateDatetime;
+
+    // 更新备注
+    private String updateNote;
+
+    // 审核人
+    private String approver;
+
+    // 审核时间
+    private Date approveDatetime;
+
+    // 审核备注
+    private String approveNote;
+
+    // 备注
+    private String remark;
+
+    // ************************db*********************
+
+    // 下单开始时间
+    private Date startDatetime;
+
+    // 下单结束时间
+    private Date endDatetime;
+
+    // 状态
+    private String statusForQuery;
+
+    // 状态
+    private List<String> statusList;
+
+    // 状态
+    private List<String> fialStatus;
+
+    private String keyword;
+
+    // 审核人名字
+    private String approveName;
+
+    // 发货人名字
+    private String deliveName;
+
+    // 更信人名字
+    private String updateName;
+
+    // 订单归属人名字
+    private String toUserName;
+
+    // 等级
+    private Integer level;
+
+    // 用户
+    private Agent agent;
+
+    // 产品
+    private Product product;
+
+    // 订单类型
+    private List<String> kindList;
+
+    // 收货人名字
+    private String signeName;
+
+    // 团队长名称
+    private String leaderName;
+
+    // 队长手机号
+    private String leaderMobile;
+
+    // 等级list
+    private List<Integer> levelList;
+
+    public Date getStartDatetime() {
+        return startDatetime;
+    }
+
+    public void setStartDatetime(Date startDatetime) {
+        this.startDatetime = startDatetime;
+    }
+
+    public Date getEndDatetime() {
+        return endDatetime;
+    }
+
+    public void setEndDatetime(Date endDatetime) {
+        this.endDatetime = endDatetime;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setIsCompanySend(String isCompanySend) {
+        this.isCompanySend = isCompanySend;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+    public String getIsCompanySend() {
+        return isCompanySend;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setSpecsCode(String specsCode) {
+        this.specsCode = specsCode;
+    }
+
+    public String getSpecsCode() {
+        return specsCode;
+    }
+
+    public void setSpecsName(String specsName) {
+        this.specsName = specsName;
+    }
+
+    public String getSpecsName() {
+        return specsName;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getToUser() {
+        return toUser;
+    }
+
+    public void setToUser(String toUser) {
+        this.toUser = toUser;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getYunfei() {
+        return yunfei;
+    }
+
+    public void setYunfei(Long yunfei) {
+        this.yunfei = yunfei;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPayType() {
+        return payType;
+    }
+
+    public void setPayType(String payType) {
+        this.payType = payType;
+    }
+
+    public String getPayGroup() {
+        return payGroup;
+    }
+
+    public void setPayGroup(String payGroup) {
+        this.payGroup = payGroup;
+    }
+
+    public Long getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(Long payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public Date getPayDatetime() {
+        return payDatetime;
+    }
+
+    public void setPayDatetime(Date payDatetime) {
+        this.payDatetime = payDatetime;
+    }
+
+    public String getPayCode() {
+        return payCode;
+    }
+
+    public void setPayCode(String payCode) {
+        this.payCode = payCode;
+    }
+
+    public List<String> getStatusList() {
+        return statusList;
+    }
+
+    public void setStatusList(List<String> statusList) {
+        this.statusList = statusList;
+    }
+
+    public String getApplyUser() {
+        return applyUser;
+    }
+
+    public void setApplyUser(String applyUser) {
+        this.applyUser = applyUser;
+    }
+
+    public String getApplyNote() {
+        return applyNote;
+    }
+
+    public void setApplyNote(String applyNote) {
+        this.applyNote = applyNote;
+    }
+
+    public Date getApplyDatetime() {
+        return applyDatetime;
+    }
+
+    public void setApplyDatetime(Date applyDatetime) {
+        this.applyDatetime = applyDatetime;
+    }
+
+    public String getSigner() {
+        return signer;
+    }
+
+    public void setSigner(String signer) {
+        this.signer = signer;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDeliver() {
+        return deliver;
+    }
+
+    public void setDeliver(String deliver) {
+        this.deliver = deliver;
+    }
+
+    public Date getDeliveDatetime() {
+        return deliveDatetime;
+    }
+
+    public void setDeliveDatetime(Date deliveDatetime) {
+        this.deliveDatetime = deliveDatetime;
+    }
+
+    public String getLogisticsCode() {
+        return logisticsCode;
+    }
+
+    public void setLogisticsCode(String logisticsCode) {
+        this.logisticsCode = logisticsCode;
+    }
+
+    public String getLogisticsCompany() {
+        return logisticsCompany;
+    }
+
+    public void setLogisticsCompany(String logisticsCompany) {
+        this.logisticsCompany = logisticsCompany;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+    public Date getUpdateDatetime() {
+        return updateDatetime;
+    }
+
+    public void setUpdateDatetime(Date updateDatetime) {
+        this.updateDatetime = updateDatetime;
+    }
+
+    public String getUpdateNote() {
+        return updateNote;
+    }
+
+    public void setUpdateNote(String updateNote) {
+        this.updateNote = updateNote;
+    }
+
+    public String getApprover() {
+        return approver;
+    }
+
+    public void setApprover(String approver) {
+        this.approver = approver;
+    }
+
+    public Date getApproveDatetime() {
+        return approveDatetime;
+    }
+
+    public void setApproveDatetime(Date approveDatetime) {
+        this.approveDatetime = approveDatetime;
+    }
+
+    public String getApproveNote() {
+        return approveNote;
+    }
+
+    public void setApproveNote(String approveNote) {
+        this.approveNote = approveNote;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getStatusForQuery() {
+        return statusForQuery;
+    }
+
+    public void setStatusForQuery(String statusForQuery) {
+        this.statusForQuery = statusForQuery;
+    }
+
+    public String getApproveName() {
+        return approveName;
+    }
+
+    public void setApproveName(String approveName) {
+        this.approveName = approveName;
+    }
+
+    public String getDeliveName() {
+        return deliveName;
+    }
+
+    public void setDeliveName(String deliveName) {
+        this.deliveName = deliveName;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName;
+    }
+
+    public String getToUserName() {
+        return toUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        this.toUserName = toUserName;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public List<String> getFialStatus() {
+        return fialStatus;
+    }
+
+    public void setFialStatus(List<String> fialStatus) {
+        this.fialStatus = fialStatus;
+    }
+
+    public List<String> getKindList() {
+        return kindList;
+    }
+
+    public void setKindList(List<String> kindList) {
+        this.kindList = kindList;
+    }
+
+    public String getProCode() {
+        return proCode;
+    }
+
+    public void setProCode(String proCode) {
+        this.proCode = proCode;
+    }
+
+    public String getSigneName() {
+        return signeName;
+    }
+
+    public void setSigneName(String signeName) {
+        this.signeName = signeName;
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+
+    public String getLeaderMobile() {
+        return leaderMobile;
+    }
+
+    public void setLeaderName(String leaderName) {
+        this.leaderName = leaderName;
+    }
+
+    public void setLeaderMobile(String leaderMobile) {
+        this.leaderMobile = leaderMobile;
+    }
+
+    public List<Integer> getLevelList() {
+        return levelList;
+    }
+
+    public void setLevelList(List<Integer> levelList) {
+        this.levelList = levelList;
+    }
+
+}
