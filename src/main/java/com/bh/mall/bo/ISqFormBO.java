5f509558c6229fe42e0d156fe56b9259b39da97d
nyc <yachao.ning@hichengdai.com>
2018-08-02 17:15:06 +0800

    升级

--- src/main/java/com/bh/mall/bo/ISqFormBO.java
+++ src/main/java/com/bh/mall/bo/ISqFormBO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.bh.mall.bo;
 
 import java.util.List;
@@ -28,34 +27,3 @@ public interface ISqFormBO extends IPaginableBO<SqForm> {
 
     public String addInfo(SqForm data);
 }
-=======
-package com.bh.mall.bo;
-
-import java.util.List;
-
-import com.bh.mall.bo.base.IPaginableBO;
-import com.bh.mall.domain.SqForm;
-
-public interface ISqFormBO extends IPaginableBO<SqForm> {
-
-    public List<SqForm> querySqFormList(SqForm condition);
-
-    public SqForm getSqForm(String code);
-
-    public List<SqForm> querySqFormPage(int start, int limit, SqForm condition);
-
-    public String approveSqForm(SqForm user);
-
-    public String cancelSqForm(SqForm data);
-
-    public String saveSqForm(SqForm data, String toUserId);
-
-    public String applySqForm(SqForm data);
-
-    public String addSqForm(SqForm data);
-
-    public String toApply(SqForm data);
-
-    public String addInfo(SqForm data);
-}
->>>>>>> refs/remotes/origin/master
